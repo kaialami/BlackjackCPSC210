ab2d4d321c997931bb2da5d9ede67017f14e9149
kai <kai@kai.com>
2022-03-02 23:33:00 -0800

    fixed code coverage in player / dealer

--- src/main/model/Dealer.java
+++ src/main/model/Dealer.java
@@ -2,8 +2,7 @@ package model;
 
 import org.json.JSONObject;
 
-// Represents the dealer with a hand and score. The dealer will hit until their score is >= 17.
-// NOTE: Dealer functions exactly as Player. Dealer was created just for naming purposes.
+// Represents the dealer with a hand and score.
 public class Dealer extends Player {
     // EFFECTS: creates instance of dealer with empty hand and score
     public Dealer() {}
@@ -11,9 +10,7 @@ public class Dealer extends Player {
     // REQUIRES: 0 <= score <= 21
     // EFFECTS: creates dealer with specified fields
     public Dealer(int score, boolean isTurn, Hand hand) {
-        this.score = score;
-        this.isTurn = isTurn;
-        this.hand = hand;
+        super(score, isTurn, hand);
     }
 
     @Override
